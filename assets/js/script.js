15abe01364bbbc599fd396d3f13b15d7e72ddaad
aharper2568 <anthonyharper568@gmail.com>
2024-05-22 12:43:24 -0400

    added functionality to light/dark button on blog.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
+// access keys for button
 const  themeSwitcher = document.querySelector('#mode-toggle')
 const container = document.querySelector('main')
 
 let mode = 'dark';
-
+// listen for click
 themeSwitcher.addEventListener('click', function () {
   if (mode === 'dark') {
     mode = 'light';
     container.setAttribute('class', 'light');
-  }
+  } // if mode is light make dark
   else {
     mode = 'dark';
     container.setAttribute('class', 'dark')
