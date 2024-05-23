86d92296e1e05806c557971817d4f1ca4d434b9e
aharper2568 <anthonyharper568@gmail.com>
2024-05-23 14:12:01 -0400

    fixed light/dark mode for header and footer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,8 @@
 // access keys for  light/dark button
 const  themeSwitcher = document.querySelector('#mode-toggle')
 const container = document.querySelector('main')
+const footer = document.querySelector('footer')
+const header = document.querySelector('header')
 
 let mode = 'dark';
 // listen for click
@@ -8,9 +10,13 @@ themeSwitcher.addEventListener('click', function () {
   if (mode === 'dark') {
     mode = 'light';
     container.setAttribute('class', 'light');
+    footer.setAttribute('class', 'light');
+    header.setAttribute('class', 'light');
   } // if mode is light make dark
   else {
     mode = 'dark';
-    container.setAttribute('class', 'dark')
+    container.setAttribute('class', 'dark');
+    footer.setAttribute('class', 'dark');
+    header.setAttribute('class', 'dark');
   }
 });
