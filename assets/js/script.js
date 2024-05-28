e53550ae6ff4862b1495eec387fc78c3ff1df375
aharper2568 <anthonyharper568@gmail.com>
2024-05-28 14:45:05 -0400

    fixed the check for empty fields, removed html added to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,21 +10,29 @@ const submitButton = document.getElementById('submit')
 submitButton.addEventListener('click', function (event){
   event.preventDefault();
 
+  const username = usernameInput.value.trim();
+  const title = titleInput.value.trim();
+  const content = contentInput.value.trim();
+
+  if (username === '' || title === '' || content === '') {
+    alert('Please complete all fields before submitting.');
+    return; // Exit the function if validation fails
+  }
   let posts = JSON.parse(localStorage.getItem('posts')) || [];
 
   const post = { 
-    userName: usernameInput.value.trim(),
-    title: titleInput.value.trim(),
-    content: contentInput.value.trim()
+    userName: username,
+    title: title,
+    content: content
 
   };
+
 
   posts.push(post);
 
   localStorage.setItem('posts', JSON.stringify(posts));
 
-
   window.location.href = 'blog.html';
-
+ 
 
 });
